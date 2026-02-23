6a419f9e05b3599547fa63aff7fa0f481f5d94a5
fathiaindiratif425-coder <fathia.indira.tif425@polban.ac.id>
2026-02-23 16:14:44 +0700

    Update Totalharga2 function for total calculation
    Refactor Totalharga2 function to calculate total using sum of harga.

---
+++ Totalharga2.py
@@ -0,0 +1,9 @@
+from Totalharga import hargatotal
+from Inputbarang import harga
+def Totalharga2(Totalharga):
+    total_akhir = sum(harga)
+    
+    for hargatotal in Totalharga:
+        total_akhir += subtotal
+    
+    return total_akhir
