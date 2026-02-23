affb3327e5288a1c285eed2f5605af8a5952765f
fironsahuleka2007-art <fironsahuleka2007@gmail.com>
2026-02-23 15:39:21 +0700

    update header

---
+++ main.py
@@ -0,0 +1,13 @@
+# from loaderJson import data_barang
+
+
+
+def main():
+    print("=== KASIR TOKO SEDERHANA ===")
+    print("==============================\n")
+    print("Desa Ciwaruga, Kecamatan Parongpong, Kabupaten Bandung Barat\n")
+    print("Selamat berbelanja di Toko Sederhana!\n")
+    print("Februari 2026\n")
+
+
+
