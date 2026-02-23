cd2d6db3b250ddce50024e4b38b12e034617ad89
Firon Fariz <fironsahuleka2007@gmail.com>
2026-02-23 21:50:23 +0700

    Delete Totalharga2.py

--- Totalharga2.py
+++
@@ -1,9 +0,0 @@
-from Totalharga import hargatotal
-from Inputbarang import harga
-def Totalharga2(hargatotal):
-    subtotal = 0
-    
-    for hargatotal in Totalharga:
-        subtotal += hargatotal
-    
-    return subtotal
