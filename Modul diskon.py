be6d029a26163189fcec281b75c5a80d30287d9e
GharinFawwaz <gharinfawwaz0103@gmail.com>
2026-02-23 15:58:39 +0700

    Add files via upload

---
+++ Modul diskon.py
@@ -0,0 +1,11 @@
+def hitung_diskon_jumat(total_belanja, hari):
+    
+    hari = hari.lower()
+    persen_diskon = 0.10
+    if hari == "jumat"
+    else 0.0
+    
+    potongan = total_belanja * persen_diskon
+    total_akhir = total_belanja - potongan
+    
+    return total_akhir, potongan
