ba8fd8d2cad312cf42fb2c5b3297dd7a14ef3d67
fironsahuleka2007-art <fironsahuleka2007@gmail.com>
2026-02-23 21:41:55 +0700

    update

--- Inputbarang.py
+++ Inputbarang.py
@@ -1,6 +1,5 @@
 def input_barang(barang_list):
     total = 0
-
     while True:
         kode = input("Kode barang (enter untuk selesai): ").upper()
         if kode == "":
@@ -11,12 +10,9 @@ def input_barang(barang_list):
             continue
 
         jumlah = int(input("Jumlah barang: "))
-        harga = barang_list[kode]["harga"]
-        nama = barang_list[kode]["nama"]
-
-        subtotal = harga * jumlah
+        subtotal = barang_list[kode]["harga"] * jumlah
         total += subtotal
 
-        print(f"+ {nama} x {jumlah} = Rp{subtotal}")
+        print(f"+ {barang_list[kode]['nama']} x {jumlah} = Rp{subtotal}")
 
     return total
